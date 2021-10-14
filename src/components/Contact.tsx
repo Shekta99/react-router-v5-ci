914f5ab76784f8b75e2ed6444a0c881ca13b0aba
Sergio Marentes <sergiomarentes705@gmail.com>
2021-10-14 15:44:35 -0500

    adding target=blank to contact link

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,9 @@ export default function Contact() {
   return (
     <>
       <h1>Contact Page</h1>
-      <a href="https://ada-school.org/">Pagina ada school</a>
+      <a href="https://ada-school.org/" target="_blank" rel="noreferrer">
+        Pagina ada school
+      </a>
     </>
   );
 }
