d32953fcf587a08085ae6af49d005aa8fff9c644
Sergio Marentes <sergiomarentes705@gmail.com>
2022-03-10 08:34:25 -0500

    fix app unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/error 404 not found/i);
+  const linkElement = screen.getByText(/Ruta base/i);
   expect(linkElement).toBeInTheDocument();
 });
